93ee26623abb4dede9022c988820dba9dab9d251
Berdia <32432981+berdia21@users.noreply.github.com>
2023-08-08 16:45:02 +0400

    add PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
+
 import { FooterComponent } from "./components/layout/footer/footer.component";
 import { HeaderComponent } from "./components/layout/header/header.component";
 import { SliderComponent } from "./components/slider/slider.component";
 import { SlotsComponent } from "./components/slots/slots.component";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: "app-root",
--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@
 // import { AppModule } from "./app/app.module";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
-import { importProvidersFrom } from "@angular/core";
+import { importProvidersFrom, isDevMode } from "@angular/core";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { AppRoutingModule } from "./app/app-routing.module";
+import { provideServiceWorker } from '@angular/service-worker';
 
 // platformBrowserDynamic()
 //   .bootstrapModule(AppModule)
@@ -19,16 +20,18 @@ bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(HttpClientModule),
     importProvidersFrom(AppRoutingModule),
-    importProvidersFrom(
-      TranslateModule.forRoot({
+    importProvidersFrom(TranslateModule.forRoot({
         loader: {
-          provide: TranslateLoader,
-          useFactory: httpTranslateLoader,
-          deps: [HttpClient],
+            provide: TranslateLoader,
+            useFactory: httpTranslateLoader,
+            deps: [HttpClient],
         },
-      })
-    ),
-  ],
+    })),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+],
 });
 
 function httpTranslateLoader(http: HttpClient) {
