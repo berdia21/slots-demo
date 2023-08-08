1b6c0f04760a8502c02ab59dc6facee5f65472f0
Berdia <32432981+berdia21@users.noreply.github.com>
2023-08-08 13:29:47 +0400

    update to standalone and angular 16.1.8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import { FooterComponent } from "./components/layout/footer/footer.component";
+import { HeaderComponent } from "./components/layout/header/header.component";
+import { SliderComponent } from "./components/slider/slider.component";
+import { SlotsComponent } from "./components/slots/slots.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  standalone: true,
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    FooterComponent,
+    SlotsComponent,
+    SliderComponent,
+  ],
 })
 export class AppComponent {
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'ka', 'ru']);
-    translate.setDefaultLang('en');
+    translate.addLangs(["en", "ka", "ru"]);
+    translate.setDefaultLang("en");
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,36 @@
 /// <reference types="@angular/localize" />
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from "./app/app.module";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+import { importProvidersFrom } from "@angular/core";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { AppRoutingModule } from "./app/app-routing.module";
 
+// platformBrowserDynamic()
+//   .bootstrapModule(AppModule)
+//   .catch((err) => console.error(err));
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule),
+    importProvidersFrom(AppRoutingModule),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpTranslateLoader,
+          deps: [HttpClient],
+        },
+      })
+    ),
+  ],
+});
+
+function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
